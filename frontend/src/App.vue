a10313e98cd24ca314a428afb289779f72b05d58
cosy247 <705194673@qq.com>
2025-03-15 17:17:59 +0800

    实现Markdown内容管理功能，添加命令和事件处理，更新前端展示

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,30 @@
 <template>
-  <div class="App">123</div>
+  <div class="App">
+    {{ markdowns }}
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { computed, ref } from 'vue';
+
+const vscode = acquireVsCodeApi();
+const markdowns = ref([]);
+const currentIndex = ref(0);
+const currentMarkdown = computed(() => markdowns.value[currentIndex.value]);
+
+window.addEventListener('message', (event) => {
+  const message = event.data;
+  if (message.command === 'markdowns') {
+    markdowns.value = message.data;
+    if (currentMarkdown) {
+      vscode.postMessage({
+        command: 'changeTitle',
+        data: currentMarkdown.value.name,
+      });
+    }
+  }
+});
+</script>
 
 <style scoped>
 .App {
--- src/index.js
+++ src/index.js
@@ -3,19 +3,43 @@ const path = require('path');
 const vscode = require('vscode');
 
 function activate(context) {
+  let webview = null;
+  let markdowns = vscode.workspace.getConfiguration('markdown-in-sider').get('markdowns');
+  if (!Array.isArray(markdowns)) {
+    markdowns = [{ name: 'start', content: '' }];
+  } else {
+    markdowns = markdowns.filter((markdown) => markdown.name && markdown.content);
+    if (markdowns.length === 0) {
+      markdowns = [{ name: 'start', content: '' }];
+    }
+  }
+
+  // 注册页面
   vscode.window.registerWebviewViewProvider('markdown-in-sider:sider', {
     resolveWebviewView(webviewView) {
-      webviewView.webview.options = { enableScripts: true };
-      webviewView.webview.html = fs.readFileSync(path.join(context.extensionPath, 'view/index.html')).toString();
-      webviewView.webview.onDidReceiveMessage(({ commend, data }) => {
-        if (commend === 'openRequstView') {
-          openRequstView(context);
-        } else if (commend === 'openFlowView') {
-          openFlowView(context);
+      webview = webviewView.webview;
+      webview.options = { enableScripts: true, retainContextWhenHidden: true };
+      webview.html = fs.readFileSync(path.join(context.extensionPath, 'view/index.html')).toString();
+      webview.postMessage({ command: 'markdowns', data: markdowns });
+      webview.onDidReceiveMessage((message) => {
+        if (message.command === 'save') {
+          vscode.workspace.getConfiguration('markdown-in-sider').update('markdowns', message.data, true);
+        } else if (message.command === 'changeTitle') {
+          webviewView.title = message.data;
         }
       });
     },
   });
+
+  // 注册按钮指令
+  context.subscriptions.push(
+    vscode.commands.registerCommand('markdown-in-sider:select', () => {
+      webview.postMessage({ command: 'select' });
+    }),
+    vscode.commands.registerCommand('markdown-in-sider:preview', () => {
+      webview.postMessage({ command: 'preview' });
+    })
+  );
 }
 
 function deactivate() {}
