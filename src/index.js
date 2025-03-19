ab733a66c9bb55b3e69493e94e019c41bffd324d
cosy247 <705194673@qq.com>
2025-03-19 13:37:46 +0800

    为Markdown编辑器添加新建、导入和删除文档功能，优化文档管理体验

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ let webviewView = null;
 let currentMarkdownIndex = 0;
 let markdowns = vscode.workspace.getConfiguration('markdown-in-sider').get('markdowns');
 if (!Array.isArray(markdowns)) {
-  markdowns = [{ name: 'start', content: '' }];
+  markdowns = [{ name: 'start', id: Date.now(), file: '', content: '# start' }];
 } else {
   markdowns = markdowns.filter((markdown) => markdown.name && markdown.content);
   if (markdowns.length === 0) {
-    markdowns = [{ name: 'start', content: '' }];
+    markdowns = [{ name: 'start', id: Date.now(), file: '', content: '# start' }];
   }
 }
 
@@ -49,71 +49,96 @@ function activate(context) {
       },
     }
   );
-
+  // type,taskid,caseid,tyid,user,seatid,pid
+  // type为0是数据回放，type为1是最小化窗口，type为2是关闭窗口
   // 注册按钮指令
   context.subscriptions.push(
-    // 选择文档或操作
+    // 新建文档
+    vscode.commands.registerCommand('markdown-in-sider:add', async () => {
+      const newOption = await vscode.window.showInputBox({
+        placeHolder: '请输入新文档名',
+        prompt: '新文档名',
+      });
+      if (newOption) {
+        markdowns.push({ name: newOption, id: Date.now(), content: `# ${newOption}` });
+        setMarkdown(markdowns.length - 1);
+        updateConfig();
+      }
+    }),
+    // 导入
+    vscode.commands.registerCommand('markdown-in-sider:reference', async () => {
+      const uris = await vscode.window.showOpenDialog({
+        saveLabel: '选择文件', // 对话框按钮文本
+        canSelectMany: true,
+      });
+      if (!uris || uris.length === 0) return;
+      // 弹出输入框让用户输入文件内容
+      for (const uri of uris) {
+        // 文档关联路径
+        const markdown = { file: uri.fsPath, id: Date.now(), name: uri.path.split('/').pop().split('.')[0] };
+        // 读取文档内容
+        try {
+          markdown.content = (await fs.readFileSync(uri.fsPath)).toString();
+          markdowns.push(markdown);
+          setMarkdown(markdowns.length - 1);
+          updateConfig();
+        } catch (error) {
+          vscode.window.showErrorMessage(`读取文件 ${fileName} 时出错:`, error);
+        }
+      }
+    }),
+    // 选择文档
     vscode.commands.registerCommand('markdown-in-sider:select', async () => {
-      const docnames = markdowns.map((m) => m.name);
-      const options = ['✨ 新建 ', '✂️ 删除 '].concat(docnames); // 添加“新建...”选项
+      const options = markdowns.map((m) => ({ label: m.name, id: m.id }));
       const selectedOption = await vscode.window.showQuickPick(options, {
         placeHolder: '选择文档或操作!',
       });
-      if (!selectedOption) return;
-      if (selectedOption === '✨ 新建 ') {
-        const newOption = await vscode.window.showInputBox({
-          placeHolder: '请输入新文档名',
-          prompt: '新文档名',
-        });
-        if (newOption) {
-          markdowns.push({ name: newOption, content: `# ${newOption}` });
-          setMarkdown(markdowns.length - 1);
-          updateConfig();
-        }
-      } else if (selectedOption === '✂️ 删除 ') {
-        const deleteDoc = await vscode.window.showQuickPick(docnames, {
-          placeHolder: '选择需要删除的文档!',
-        });
-        if (deleteDoc) {
-          const deleteIndex = markdowns.findIndex((m) => m.name === deleteDoc);
-          if (markdowns.length === 1) {
-            markdowns = [{ name: 'start', content: '' }];
-          } else {
-            markdowns = markdowns.splice(deleteIndex, 1);
-          }
-          if (deleteIndex === currentMarkdownIndex) {
-            setMarkdown(0);
-          } else if (deleteIndex < currentMarkdownIndex) {
-            currentMarkdownIndex--;
-          }
-          updateConfig();
-        }
-      } else if (markdowns[currentMarkdownIndex].name !== selectedOption) {
-        const docIndex = markdowns.findIndex((m) => m.name === selectedOption);
+      if (selectedOption && markdowns[currentMarkdownIndex].id !== selectedOption.id) {
+        const docIndex = markdowns.findIndex((m) => m.id === selectedOption.id);
         setMarkdown(docIndex);
+      }
+    }),
+    // 删除文档
+    vscode.commands.registerCommand('markdown-in-sider:delete', async () => {
+      const options = markdowns.map((m) => ({ label: m.name, id: m.id }));
+      const deleteDoc = await vscode.window.showQuickPick(options, {
+        placeHolder: '选择需要删除的文档!',
+      });
+      if (deleteDoc) {
+        const deleteIndex = markdowns.findIndex((m) => m.id === deleteDoc.id);
+        if (markdowns.length === 1) {
+          markdowns = [{ name: 'start', id: Date.now(), content: '# start' }];
+        } else {
+          markdowns.splice(deleteIndex, 1);
+        }
+        if (deleteIndex === currentMarkdownIndex) {
+          setMarkdown(0);
+        } else if (deleteIndex < currentMarkdownIndex) {
+          currentMarkdownIndex--;
+        }
+        updateConfig();
+      }
+    }),
+    // 另存为
+    vscode.commands.registerCommand('markdown-in-sider:save', async () => {
+      const markdown = markdowns[currentMarkdownIndex];
+      const url = await vscode.window.showSaveDialog({
+        saveLabel: '保存', // 对话框按钮文本
+        defaultUri: vscode.Uri.file(markdown.file || `${markdown.name}.md`),
+      });
+      if (!url) return;
+      // 弹出输入框让用户输入文件内容
+      try {
+        markdown.file = url.fsPath;
+        await vscode.workspace.fs.writeFile(url, Buffer.from(markdown.content, 'utf8'));
+        updateConfig();
+      } catch (error) {
+        vscode.window.showErrorMessage(`Failed to save file: ${error.message}`);
       }
     }),
     // 预览
     vscode.commands.registerCommand('markdown-in-sider:preview', () => {
       webviewView.webview.postMessage({ command: 'preview' });
-    }),
-    // 另存为
-    vscode.commands.registerCommand('markdown-in-sider:save', async () => {
-      const { name, content } = markdowns[currentMarkdownIndex];
-      const url = await vscode.window.showSaveDialog(
-        (options = {
-          saveLabel: '保存', // 对话框按钮文本
-          filters: [{ name: 'Markdown Files', extensions: ['.md'] }],
-          defaultUri: vscode.Uri.file(`${name}.md`),
-        })
-      );
-      if (!url) return;
-      // 弹出输入框让用户输入文件内容
-      try {
-        await vscode.workspace.fs.writeFile(url, Buffer.from(content, 'utf8'));
-      } catch (error) {
-        vscode.window.showErrorMessage(`Failed to save file: ${error.message}`);
-      }
     })
   );
 }
